c584770361539d692037668fc38dbd270c581b02
nomsoani <nomsoani5000@gmail.com>
2023-08-17 19:39:55 +0100

    update

---
+++ script.js
@@ -0,0 +1,37 @@
+// navbar toggle action 
+$(window).ready(function(){
+    $(".boton").wrapInner('<div class=botontext></div>');
+        
+        $(".botontext").clone().appendTo( $(".boton") );
+        
+        $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');
+        
+        $(".twist").css("width", "25%").css("width", "+=3px");
+    });
+
+//       hero-section display
+
+var card = $(".card");
+
+$(document).on("mousemove",function(e) { 
+  var ax = -($(window).innerWidth()/2- e.pageX)/20;
+  var ay = ($(window).innerHeight()/2- e.pageY)/10;
+  card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg);-moz-transform: rotateY rotateX("+ay+"deg)");
+});
+
+
+// counter box  
+
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 3500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
